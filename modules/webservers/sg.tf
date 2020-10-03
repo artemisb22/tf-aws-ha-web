a7903f340df1300d3f159649160a377b64219ab1
artemisb22 <44167906+artemisb22@users.noreply.github.com>
2020-10-03 20:08:42 +0200

    Update sg.tf

--- modules/webservers/sg.tf
+++ modules/webservers/sg.tf
@@ -4,7 +4,8 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_default_security_group" "default" {
+resource "aws_security_group" "default" {
+  name        = "default"
   vpc_id = data.aws_vpc.default.id
 
     egress {
