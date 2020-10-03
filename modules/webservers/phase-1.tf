2dcc985842d2781ea82d64f6786e04e6082fe89a
artemisb22 <44167906+artemisb22@users.noreply.github.com>
2020-10-03 19:12:19 +0200

    Add files via upload

---
+++ modules/webservers/phase-1.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "ph-1-web_nodes" {
+  count                       = var.web_nodes_count
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  user_data                   = data.template_file.ph1-web.rendered
+  subnet_id                   = data.aws_subnet_ids.default.id
+  key_name                    = var.key_name
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name        = "ph-1-web-${count.index + 1}"
+    Provisioner = "terraform"
+    Role        = "web"
+    Service     = "blabla.everc.com"
+  }
+
+}
+
+data "template_file" "ph1-web" {
+  template = file( "${ path.module }/user-data/ph-1-web.sh" )
+  vars = {
+    SERVICE-1= lookup(aws_instance.ph-1-web_nodes.tags, "Service")
+  }
+}
