442bb132c14a3bd10a3841b239c1c8934607db12
artemisb22 <44167906+artemisb22@users.noreply.github.com>
2020-10-03 20:44:35 +0200

    Update phase-1.tf

--- modules/webservers/phase-1.tf
+++ modules/webservers/phase-1.tf
@@ -1,9 +1,18 @@
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default" {
+  for_each = data.aws_subnet_ids.default.ids
+  id       = each.value
+}
+
 resource "aws_instance" "ph-1-web_nodes" {
   count                       = var.web_nodes_count
   ami                         = var.ami_id
   instance_type               = var.instance_type
   user_data                   = data.template_file.ph1-web.rendered
-  subnet_id                   = aws_default_subnet.default.id
+  subnet_id                   = aws_subnet.default.id
   key_name                    = var.key_name
   root_block_device {
     volume_size = var.root_volume_size
