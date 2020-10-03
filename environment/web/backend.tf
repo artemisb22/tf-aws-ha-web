4247a409068f13713629ef87841023ccf4714b2f
artemisb22 <44167906+artemisb22@users.noreply.github.com>
2020-10-03 20:04:18 +0200

    Update backend.tf

--- environment/web/backend.tf
+++ environment/web/backend.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket = "tf-bucket-lab"
     region = "eu-west-3"
-    key = "tf-lab.tfstate"
+    key = "tf/tf-lab.tfstate"
 #    dynamodb_table = "terraform-state-lock-dynamo"
     encrypt = true
   }
@@ -17,28 +17,4 @@ terraform {
 
 provider "aws" {
   region      = var.aws_region
-}
-
-resource "aws_s3_bucket" "terraform-state-s3" {
-  bucket = "tf-bucket-lab"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
 }
